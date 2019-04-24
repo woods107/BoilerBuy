5d90a783af27a4f1bc2291f69c47091af109268d
Michael Woods <woods107@purdue.edu>
2019-04-24 17:00:31 -0400

    Began simple handling of dynamic content by testing with placeholders Fixed issue of templates and static folders not being found correctly

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-        return "Bottom Text"
+    #Use placeholder dummy list of posts for the moment
+    #User object will contain username, real name, and contact info fields (eventually)
+    #Posts will contain title, description, posting date, object of user, and a photo
+    postings = [
+            {
+                'user': {'name': 'jane'},
+                'title': 'Test Title 1', 
+                'desc': 'Test Description 1'
+            },        
+            {
+                'user': {'name': 'joseph'},
+                'title': 'Test Title 2',
+                'desc': 'Test Description 2'
+            }
+    ]
+    return render_template('index.html', postings=postings)
