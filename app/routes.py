32fb2dfbc3305b7f1e62964ff8365dba09c3632f
Michael Woods <woods107@purdue.edu>
2019-04-25 14:01:34 -0400

    Added basic forms functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -20,3 +21,13 @@ def index():
             }
     ]
     return render_template('index.html', postings=postings)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(
+            form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
