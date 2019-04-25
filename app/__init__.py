31d21b321aa307386fdfcbde71dd8719075ca5cd
Michael Woods <woods107@purdue.edu>
2019-04-25 14:05:23 -0400

    Cleaned up file structure slightly

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app = Flask(__name__)
 app.config.from_object(Config)
 
 from app import routes
