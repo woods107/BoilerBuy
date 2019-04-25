1a080f5296543f4d88315f4b97fa42d4e6a29091
Piper <will1742@purdue.edu>
2019-04-25 17:12:09 -0400

    Added listing schema

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,5 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# include models to organize dbs
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 # defines user within db
@@ -6,6 +7,20 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # links user to Posts
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+# defines listings within db
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(256))
+    # allows us to list posts chronologically
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # allows us to attach users to their posts
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
