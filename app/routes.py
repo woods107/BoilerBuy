085eee8d16f07ca74cb2756074459eb1c6168da5
Michael Woods <woods107@purdue.edu>
2019-04-25 23:21:13 -0400

    Began simple update of index page to use database values

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
 from app.forms import LoginForm
 from app.forms import RegisterForm
+from app.models import User, Post
 
 @app.route('/')
 @app.route('/index')
@@ -9,18 +10,7 @@ def index():
     #Use placeholder dummy list of posts for the moment
     #User object will contain username, real name, and contact info fields (eventually)
     #Posts will contain title, description, posting date, object of user, and a photo
-    postings = [
-            {
-                'user': {'name': 'jane'},
-                'title': 'Test Title 1',
-                'desc': 'Test Description 1'
-            },
-            {
-                'user': {'name': 'joseph'},
-                'title': 'Test Title 2',
-                'desc': 'Test Description 2'
-            }
-    ]
+    postings = Post.query.all()#= []
     return render_template('index.html', postings=postings)
 
 
